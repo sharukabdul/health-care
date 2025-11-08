4c6f887982fc7af8839d5993943f648ed0d6a230
sharukabdul <sharukabdul786@gmail.com>
2025-11-08 13:29:50 +0530

    routing setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
+import MyProfile from './components/dashboard/MyProfile';
+import WelnessGoals from './components/dashboard/WelnessGoals';
+import Messages from './components/dashboard/Messages';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>Health Care</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/my-profile" element={<MyProfile />} />
+          <Route path="/welness-goals" element={<WelnessGoals />} />
+          <Route path="/messages" element={<Messages />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/dashboard/WelnessGoals.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WelnessGoals() {
+  return (
+    <div>
+      <h2>WelnessGoals</h2>
+    </div>
+  );
+};
